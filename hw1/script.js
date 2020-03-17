2e6ddd7232e53a6d8839b7910d0edf56492c1a7b
vvvmvvv <boxingvm16@gmail.com>
2020-03-18 00:01:27 +0200

    add response error

--- hw1/script.js
+++ hw1/script.js
@@ -1,19 +1,19 @@
 const http = require('http');
 const fs = require('fs');
-
 const PATH = './requestInfo.json';
 
 http.createServer((request, response) => {
     const {method, url} = request;
     response.writeHead(200, {'Content-type': 'application/json'});
     const {statusMessage : status} = response;
-    response.end(JSON.stringify({status}));
     const data = {status, method, url, time: Date.now()};
-    addJson(data);
+    addJson(data, response);
+    response.end(JSON.stringify({status}));
 }).listen(8081);
 
-let addJson = (data) => {
+let addJson = (data, response) => {
     try {
+        throw new Error("ERROR!!!");
         let parsed = {logs: []};
 
         if (fs.existsSync(PATH)) {
@@ -29,12 +29,10 @@ let addJson = (data) => {
         parsed.logs.push(data);
 
         parsed = JSON.stringify(parsed, null, 4);
-        fs.writeFileSync(PATH, parsed,  'utf8');        
+        fs.writeFileSync(PATH, parsed,  'utf8');
     } catch (error) {
-        console.error(error);
+        response.writeHead(500, {'Content-type': 'application/json'})
+        response.end(JSON.stringify({error}));
+        //process.exit(1);
     }
-}
-
-let queryRange = () => {
-
 }
